31f59fde41733729557985da02f4c1b1e2ceb42d
kwinsi <sadhukwinsi25@gmail.com>
2021-10-01 05:29:59 -0400

    Changed Text Alignment.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-	return <div style={{ textAlign: "center" }}>EduHub</div>;
+	return <div style={{ textAlign: "left" }}>EduHub</div>;
 }
 
 export default App;

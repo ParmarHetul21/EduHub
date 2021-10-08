4e86cb5a289c8870a87bbd10ea19636828ccc4c3
imark94179 <83642523+imark94179@users.noreply.github.com>
2021-10-08 17:52:49 +0530

    admin page done.

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,69 +1,88 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
-import {
-	Navbar,
-	Nav,
-	NavDropdown,
-	Button,
-	Form,
-	FormControl,
-} from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Nav } from "react-bootstrap";
+import Dropdown from "react-bootstrap/Dropdown";
 import EduHub from "../components/icons/Main_Logo.png";
+import UserIcon from "../components/icons/user_white.png";
 import "../App.css";
 
 class Header extends Component {
-	render() {
-		return (
-			<div>
-				<Navbar className="header" variant="dark" fixed="top">
-					<Navbar.Brand href="#home" className="p-1">
-						<img
-							src={EduHub}
-							width="150"
-							height="60"
-							className="d-inline-block align-top"
-							alt="Logo"
-						/>
-					</Navbar.Brand>
+  state = {
+    isAdmin: true,
+  };
+  render() {
+    return (
+      <div>
+        <Navbar className="header" variant="dark" fixed="top">
+          <Navbar.Brand className="p-1">
+            <Link to="/">
+              <img
+                src={EduHub}
+                width="150"
+                height="60"
+                className="d-inline-block align-top"
+                alt="Logo"
+              />
+            </Link>
+          </Navbar.Brand>
 
-					<Navbar.Toggle aria-controls="basic-navbar-nav" />
-					<Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
-					<Nav className="mr-auto">
-						<Link
-							className="h5 text-light text-decoration-none"
-							to="/"
-						>
-							Home
-						</Link>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
 
-						<Link
-							className="px-4 h5 text-light text-decoration-none"
-							to="/login"
-						>
-							Login
-						</Link>
-						{/* <NavDropdown
-                className="h5 text-light"
-                title="Dropdown"
-                id="basic-nav-dropdown"
+          <Nav className="mr-auto">
+            <Link
+              className="h5 text-light px-3 text-decoration-none py-4"
+              to="/"
+            >
+              Home
+            </Link>
+            {this.state.isAdmin ? (
+              <>
+                <Link
+                  className="h5 px-3 text-light  text-decoration-none py-4"
+                  to="/"
+                >
+                  Inquiries
+                </Link>
+                <Dropdown>
+                  <Dropdown.Toggle
+                    variant="success"
+                    id="dropdown-basic"
+                    style={{
+                      backgroundColor: "transparent",
+                      border: "none",
+                      marginTop: "17px",
+                      marginRight: "10px",
+                    }}
+                  >
+                    <img src={UserIcon} height="30px" width="30px" alt="User" />
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu style={{ marginLeft: "-100px" }}>
+                    <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+                    <Dropdown.Item href="#/action-2">
+                      Another action
+                    </Dropdown.Item>
+                    <Dropdown.Item href="#/action-3">
+                      Something else
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </>
+            ) : (
+              <Link
+                className="px-4 h5 text-light text-decoration-none"
+                to="/login"
               >
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown> */}
-					</Nav>
-				</Navbar>
-			</div>
-		);
-	}
+                Login
+              </Link>
+            )}
+          </Nav>
+        </Navbar>
+      </div>
+    );
+  }
 }
 
 export default Header;
